5a62b3610b978ceea2dd91a58899085377047364
Jean Baptiste MUGISHA <johnbutista004@gmail.com>
2026-04-15 15:28:54 +0200

    feat: add van components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Vans from "./components/Vans";
 
 function App() {
   return (
     <>
       <Header />
+      <Vans/>
     </>
   );
 }
---
+++ src/components/Vans.tsx
@@ -0,0 +1,8 @@
+
+export default function Vans() {
+  return (
+    <div>
+      <h1>Vans goes here</h1>
+    </div>
+  )
+}
