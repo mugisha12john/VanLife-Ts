5619b06c21e18a0f65332cf900ee894d9de2740e
Jean Baptiste MUGISHA <johnbutista004@gmail.com>
2026-04-16 16:44:56 +0200

    feat: add react router dom depencies and also add links in header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,17 @@
+import { BrowserRouter, Link } from "react-router-dom";
+
 export default function Header() {
   return (
     <div>
-      <header className="flex justify-around mt-10">
-        <h1 className="font-black text-2xl">#VANLIFE</h1>
-        <div className="flex gap-10">
-          <li>About</li>
-          <li>Vans</li>
-        </div>
-      </header>
+      <BrowserRouter>
+        <header className="flex justify-around mt-10">
+          <h1 className="font-black text-2xl">#VANLIFE</h1>
+          <div className="flex gap-10">
+            <Link to="/about">About</Link>
+            <Link to="/vans">Vans</Link>
+          </div>
+        </header>
+      </BrowserRouter>
     </div>
   );
-}   
+}
