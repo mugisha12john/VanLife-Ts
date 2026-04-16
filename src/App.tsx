1e911690d8c8f9376730cc5933ecd0d5feb32f83
Jean Baptiste MUGISHA <johnbutista004@gmail.com>
2026-04-16 16:32:45 +0200

    feat: create simple server

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import Vans from "./components/Vans";
-
+import "../server"; // make sure this runs before <App />
 function App() {
   return (
     <>
--- src/components/Vans.tsx
+++ src/components/Vans.tsx
@@ -1,8 +1,26 @@
-
+import React from "react";
 export default function Vans() {
+  const [vans, setVans] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("/api/vans")
+      .then((res) => res.json())
+      .then((data) => setVans(data.vans));
+  }, []);
+
   return (
     <div>
-      <h1>Vans goes here</h1>
+      <h1>Vans</h1>
+      <ul>
+        {vans.map((van) => (
+          <li key={van.id}>
+            <img src={van.imageUrl} alt={van.name} width={100} />
+            <p>
+              {van.name} – ${van.price}/day
+            </p>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
