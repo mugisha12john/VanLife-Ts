aaefafd105641f472957ea700d5f620552c0a85f
Jean Baptiste MUGISHA <johnbutista004@gmail.com>
2026-04-20 11:52:05 +0200

    feat: van detail page

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "./components/Home";
 import About from "./components/About";
+import VanDetail from "./components/VanDetail";
 function App() {
   return (
     <>
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Home />}></Route>
           <Route path="/vans" element={<Vans />}></Route>
           <Route path="/about" element={<About/>}></Route>
+          <Route path="/vans/:id" element={<VanDetail/>}></Route>
 
         </Routes>
       </BrowserRouter>
---
+++ src/components/VanDetail.tsx
@@ -0,0 +1,8 @@
+
+export default function VanDetail() {
+  return (
+    <div>
+      <h1>Van detail goes here!</h1>
+    </div>
+  )
+}
