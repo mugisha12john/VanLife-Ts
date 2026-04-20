8498beb0ec490eb9f41745f96e271adbb79cdfd0
Jean Baptiste MUGISHA <johnbutista004@gmail.com>
2026-04-20 14:41:46 +0200

    feat: work on van detail

--- src/components/VanDetail.tsx
+++ src/components/VanDetail.tsx
@@ -9,19 +9,22 @@ export default function VanDetail() {
       .then((res) => res.json())
       .then((data) => setVans(data));
   }, [params.id]);
-  
+
   return (
-    <div className="van-detail-container">
+    <div className="van-detail-container flex justify-center items-center flex-col ml-[30%] mt-10">
       {van ? (
-        <div className="van-detail">
-          <img src={van.vans.imageUrl} />
-          <i className={`van-type ${van.vans.type} selected`}>{van.vans.type}</i>
-          <h2>{van.vans.name}</h2>
-          <p className="van-price">
-            <span>${van.vans.price}</span>/day
+        <div className="van-detail flex  items-start flex-col">
+          <img src={van.vans.imageUrl} className="w-1/2 rounded-xl" />
+          <i className="bg-[#E17654] p-2 w-25 rounded-xl font-medium text-white mt-5">
+            {van.vans.type}
+          </i>
+          <h2 className="text-3xl font-bold">{van.vans.name}</h2>
+          <p className="  w-96 mt-2">
+            <span className=" font-semibold text-xl">${van.vans.price}</span>
+            /day
           </p>
-          <p>{van.vans.description}</p>
-          <button className="link-button">Rent this van</button>
+          <p className=" font-400 text-lg w-125">{van.vans.description}</p>
+          <button className="bg-[#FF8C38] text-white font-bold text-lg w-125 mt-4 p-2 text-center rounded-2xl">Rent this van</button>
         </div>
       ) : (
         <h2>Loading...</h2>
