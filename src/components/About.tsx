222fc2634f071c2af5f71e9e430b6077359014ae
Jean Baptiste MUGISHA <johnbutista004@gmail.com>
2026-04-20 10:41:06 +0200

    feat: work on about footer

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -25,6 +25,9 @@ export default function About() {
           Explore our vans
         </button>
       </div>
+      <div className="mt-5 bg-[#252525] w-125 flex justify-center p-2">
+        <p className="text-[#AAAAAA]">Ⓒ 2022 #VANLIFE</p>
+      </div>
     </div>
   );
 }
