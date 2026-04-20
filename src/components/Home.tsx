1c474f805037157ab3d2bb8b2eba82d061be5b6d
Jean Baptiste MUGISHA <johnbutista004@gmail.com>
2026-04-20 11:20:43 +0200

    feat: home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,17 +7,24 @@ export default function Home() {
     <>
       <div
         style={{ backgroundImage: `url(${bg})` }}
-        className="max-w-[90vw] ml-20  h-[85vh] bg-cover bg-center   flex justify-center items-center flex-col mt-10"
+        className="max-w-[90vw] ml-20  h-[85vh] bg-cover bg-center flex flex-col items-center justify-center   mt-10"
       >
-        <h1>You got the travel plans, we got the travel vans.</h1>
-        <p>
+        <h1 className="max-w-125 text-white font-bold text-4xl">
+          You got the travel plans, we got the travel vans.
+        </h1>
+        <p className="font-medium text-xl text-white mt-10 w-140">
           Add adventure to your life by joining the #vanlife movement. Rent the
           perfect van to make your perfect road trip.
         </p>
-        <Link to="/vans">Find your van</Link>
+        <Link
+          to="/vans"
+          className="bg-[#FF8C38] text-white font-bold text-lg w-125 mt-4 p-2 text-center rounded-2xl"
+        >
+          Find your van
+        </Link>
       </div>
       <div className=" bg-[#252525] max-w-[90vw] ml-20 flex justify-center p-2 mb-5">
-        <p className="text-[#AAAAAA]">Ⓒ 2022 #VANLIFE</p>
+        <p className="text-[#AAAAAA] ">Ⓒ 2022 #VANLIFE</p>
       </div>
     </>
   );
