cc822531856fcd536060006d5a59f51ea20cc551
Jean Baptiste MUGISHA <johnbutista004@gmail.com>
2026-04-22 15:05:20 +0200

    feat: work on host van pricing

--- src/components/host/HostVanPricing.tsx
+++ src/components/host/HostVanPricing.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
-
+import { useOutletContext } from "react-router-dom";
+import type { Van } from "../../../server";
 export default function HostVanPricing() {
+  const van = useOutletContext<Van[]>();
   return (
-    <div>
-      Host pricing goes here
+    <div className="mt-5 flex justify-center items-center">
+      <h3 className="text-2xl font-semibold">${van[0].price}</h3>
+      <p>/day</p>
     </div>
-  )
+  );
 }
