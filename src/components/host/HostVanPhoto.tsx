e12521ddb931528cb9db295169a95b545a103b45
Jean Baptiste MUGISHA <johnbutista004@gmail.com>
2026-04-22 15:08:10 +0200

    feat: work on host photo and all of this we use Outlet context to implement that

--- src/components/host/HostVanPhoto.tsx
+++ src/components/host/HostVanPhoto.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
-
+import { useOutletContext } from "react-router-dom"
+import type { Van } from "../../../server"
 export default function HostVanPhoto() {
+    const van = useOutletContext<Van[]>()
   return (
     <div>
-      <h1>Host photo goes here</h1>
+      <img src={van[0].imageUrl} alt={van[0].name} className="w-40 mt-4 rounded-2xl"/>
     </div>
   )
 }
