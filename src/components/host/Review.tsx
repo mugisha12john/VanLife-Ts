10576c9f1dd66eaabaf0eebd28828de033520abd
Jean Baptiste MUGISHA <johnbutista004@gmail.com>
2026-04-24 16:56:04 +0200

    feat: work on review Ui

--- src/components/host/Review.tsx
+++ src/components/host/Review.tsx
@@ -1,7 +1,14 @@
+import rating from "../../assets/rating.png";
 export default function Review() {
   return (
-    <div>
-      <h1>Review van goes here</h1>
+    <div className="flex flex-col justify-center items-center mt-5">
+      <div className="flex gap-5 items-center">
+        <h1 className="font-bold text-3xl">Your reviews</h1>
+        <p>
+          last <u>30 days</u>
+        </p>
+      </div>
+      <img src={rating} alt="review ratings" className="max-w-2xl h-137" />
     </div>
   );
 }
