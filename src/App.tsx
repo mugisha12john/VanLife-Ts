75c348205c898f89b16b6940990e7d034c87ee7a
Jean Baptiste MUGISHA <johnbutista004@gmail.com>
2026-04-24 11:06:54 +0200

    feat: work with loader and error, also i add footer and layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
 import Vans from "./components/Vans";
 import "../server"; // make sure this runs before <App />
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-// import Task1 from "./components/Task1";
-
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import VanDetail from "./components/VanDetail";
@@ -18,28 +20,31 @@ import HostVanInfo from "./components/host/HostVanInfo";
 import HostVanPhoto from "./components/host/HostVanPhoto";
 import HostVanPricing from "./components/host/HostVanPricing";
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Header />}>
+        <Route path="home" element={<Home />} />
+        <Route path="vans" element={<Vans />} />
+        <Route path="about" element={<About />} />
+        <Route path="vans/:id" element={<VanDetail />} />
+
+        <Route path="host" element={<HostLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="income" element={<Income />} />
+          <Route path="review" element={<Review />} />
+          <Route path="van" element={<HostVan />} />
+          <Route path="van/:id" element={<HostVanDetail />}>
+            <Route index element={<HostVanInfo />} />
+            <Route path="photo" element={<HostVanPhoto />} />
+            <Route path="pricing" element={<HostVanPricing />} />
+          </Route>
+        </Route>
+      </Route>,
+    ),
+  );
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/vans" element={<Vans />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/vans/:id" element={<VanDetail />}></Route>
-          <Route path="host" element={<HostLayout />}>
-            <Route index element={<Dashboard />}></Route>
-            <Route path="income" element={<Income />}></Route>
-            <Route path="review" element={<Review />}></Route>
-            <Route path="van" element={<HostVan />}></Route>
-            <Route path="van/:id" element={<HostVanDetail />}>
-              <Route index element={<HostVanInfo />}></Route>
-              <Route path="photo" element={<HostVanPhoto />}></Route>
-              <Route path="pricing" element={<HostVanPricing />}></Route>
-            </Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+
+export default function Footer() {
+  return (
+    <div>
+      <h1>Footer goes here</h1>
+    </div>
+  )
+}
