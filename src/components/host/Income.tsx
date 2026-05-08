dd8362e40b61efcbcb31bbcb43016ba2b27266e9
Jean Baptiste MUGISHA <johnbutista004@gmail.com>
2026-05-08 19:47:47 +0200

    feat: finiliaze income page and also add image folder for all images

--- src/components/host/Income.tsx
+++ src/components/host/Income.tsx
@@ -1,4 +1,8 @@
 import income from "../../assets/income.png";
+import g1 from "../../assets/image/g1.png";
+import g2 from "../../assets/image/g2.png";
+import g3 from "../../assets/image/g3.png";
+import transaction from "../../assets/image/transaction.png";
 export default function Income() {
   return (
     <div className="flex justify-center flex-col items-center mt-10">
@@ -15,6 +19,12 @@ export default function Income() {
         <h2 className="text-5xl font-black m-4">$ 2,230</h2>
       </div>
       <img src={income} alt="income graph" />
+      <div className="mt-4">
+        <img src={transaction} alt="transaction image" />
+        <img src={g1} alt="transaction 1" />
+        <img src={g2} alt="transaction 2" />
+        <img src={g3} alt="transaction 3" />
+      </div>
     </div>
   );
 }
